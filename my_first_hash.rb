d2ecb6e7c29fabba829189cb867b80cd14d02851
Marc Mills <mdmills1990@gmail.com>
2018-08-24 16:36:02 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,14 +31,6 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest["muskets"] = "2"
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2, shipping_manifest["gun powder"] = 4
+return shipping_manifest
 end
